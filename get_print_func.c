f2722673514fa241cf7471dd738555f02a16d1d2
Kenneth1234 <kenich94@gmail.com>
2022-04-18 09:55:54 +0000

    [Feature #11]: uploading a function that prints integers

--- get_print_func.c
+++ get_print_func.c
@@ -4,22 +4,23 @@
 /**
  * get_print_func - selects the correct function to perform the operation.
  * @s: argument indentifier
- * @index: index for argument identifier
- * Return: the pointer to a function.
+ * Return: pointer to a function.
  */
 
-int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int)
+int (*get_print_func(char s))(va_list, char *, unsigned int)
 {
 	print_t pr[] = {
 		{"c", print_chr},
 		{"s", print_str},
+		{"i", print_int},
+		{"d", print_int},
 		{NULL, NULL},
 	};
 	int i = 0;
 
 	while (pr[i].type_arg)
 	{
-		if (s[index] == pr[i].type_arg[0])
+		if (s == pr[i].type_arg[0])
 			break;
 		i++;
 	}
